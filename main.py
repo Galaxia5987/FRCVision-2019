c68860ccb476010452a64e72fafbdcbbf17a5af5
Unknown <thetechproject2014@gmail.com>
2018-10-25 18:58:54 +0300

    Call functions from target

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import cv2
+
+import utils
 from display import Display
 from trackbars import Trackbars
 
@@ -9,4 +12,18 @@ trackbars = Trackbars(name)
 
 while True:
     frame = display.get_frame()
-    frame = target.pre_processing(frame)
+    # Seperate frames for display purposes
+    original = frame.copy()
+    contour_image = frame.copy()
+    # Target functions
+    mask = target.create_mask(frame)
+    contours = target.find_contours(mask)
+    filtered_contours = target.filter_contours(contours)
+    # Draw contours
+    target.draw_contours(filtered_contours, contour_image)
+    # Display
+    display.show_frame(contour_image)
+    display.show_frame(utils.bitwise_mask(original, mask), title="mask")
+    k = cv2.waitKey(1) & 0xFF  # large wait time to remove freezing
+    if k in (27, 113):
+        break
