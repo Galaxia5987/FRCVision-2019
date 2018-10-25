924736e74ef83f53042d394cc7cd95232e19d03b
Unknown <thetechproject2014@gmail.com>
2018-10-25 18:59:40 +0300

    Adapt to changes

--- trackbars.py
+++ trackbars.py
@@ -1,6 +1,3 @@
-import json
-import os
-
 import cv2
 
 import utils
@@ -8,37 +5,26 @@ from display import Display
 
 
 class Trackbars:
-    def __init__(self, file):
-        self.default = {"H": (0, 255), "S": (0, 255), "V": (0, 255)}
-        self.file = file
+    def __init__(self, name):
+        self.name = name
         self.create_trackbars()
 
-    def get_filename(self):
-        return "hsv/{}.json".format(self.file)
-
-    def load_file(self):
-        if not os.path.isfile(self.get_filename()):
-            return self.default
-        with open(self.get_filename(), "r") as f:
-            return json.load(f)
-
-    def save_file(self, value):
-        with open(self.get_filename(), "w") as f:
-            json.dump(self.get_hsv(), f)
+    def callback(self, value):
+        utils.save_file(self.name, self.get_hsv())
 
     def create_trackbars(self):
         cv2.namedWindow("HSV")
 
-        hsv = self.load_file()
+        hsv = utils.load_file(self.name)
         # create trackbars for color change
-        cv2.createTrackbar('lowH', 'HSV', hsv['H'][0], 179, self.save_file)
-        cv2.createTrackbar('highH', 'HSV', hsv['H'][1], 179, self.save_file)
+        cv2.createTrackbar('lowH', 'HSV', hsv['H'][0], 179, self.callback)
+        cv2.createTrackbar('highH', 'HSV', hsv['H'][1], 179, self.callback)
 
-        cv2.createTrackbar('lowS', 'HSV', hsv['S'][0], 255, self.save_file)
-        cv2.createTrackbar('highS', 'HSV', hsv['S'][1], 255, self.save_file)
+        cv2.createTrackbar('lowS', 'HSV', hsv['S'][0], 255, self.callback)
+        cv2.createTrackbar('highS', 'HSV', hsv['S'][1], 255, self.callback)
 
-        cv2.createTrackbar('lowV', 'HSV', hsv['V'][0], 255, self.save_file)
-        cv2.createTrackbar('highV', 'HSV', hsv['V'][1], 255, self.save_file)
+        cv2.createTrackbar('lowV', 'HSV', hsv['V'][0], 255, self.callback)
+        cv2.createTrackbar('highV', 'HSV', hsv['V'][1], 255, self.callback)
 
     def get_hsv(self):
         lowH = cv2.getTrackbarPos('lowH', 'HSV')
@@ -55,7 +41,7 @@ def run(target):
     trackbars = Trackbars(target)
     while True:
         frame = display.get_frame()
-        mask = utils.mask(frame, trackbars.get_hsv())
+        mask = utils.hsv_mask(frame, trackbars.get_hsv())
         frame = cv2.bitwise_and(frame, frame, mask=mask)
         display.show_frame(frame)
         k = cv2.waitKey(100) & 0xFF  # Wait time to remove freezing
