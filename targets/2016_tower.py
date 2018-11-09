247370e0dfaee5cd7c1d263f5f5794048ec28e8d
yuval senderovitz <yuvalsan4545@gmail.com>
2018-11-09 14:18:46 +0200

    Add base functions

---
+++ targets/2016_tower.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+import utils
+
+class Target:
+    def __init__(self, name):
+        self.name = name
+        self.kernel = np.array([[0, 1, 0],
+                                [1, 1, 1],
+                                [0, 1, 0]], dtype=np.uint8)
+
+    def create_mask(self, frame, hsv):
+        mask = utils.hsv_mask(frame, hsv)
+        # create a cross kernel
+        mask = utils.morphology(mask, self.kernel)
+        mask = cv2.threshold(mask, 127, 255, 0)[1]
+        return mask
+
+    @staticmethod
+    def find_contours(mask):
+        im2, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        return contours
+
+    def filter_contours(self, contours):
+        if not contours:
+            return
+        
