8de33886c5ee32cd9167b0e63f63c5eccebb59a5
Repulser <thetechproject2014@gmail.com>
2018-11-16 10:40:04 +0200

    Add comments fo file handler

--- file.py
+++ file.py
@@ -3,18 +3,40 @@ import os
 
 
 class File:
-    def __init__(self, default, folder):
+    """Handles reading and writing JSON files."""
+
+    def __init__(self, default: function, folder: str):
+        """
+        :param default: Function providing default value
+        :param folder: Folder to write files to
+        """
         self.default = default
         self.folder = folder
 
-    def get_filename(self, name):
+    def get_filename(self, name: str):
+        """
+        Formats filename.
+        :param name: Filename
+        :return:
+        """
         return f'{self.folder}/{name}.json'
 
-    def save_file(self, name, data):
+    def save_file(self, name: str, data):
+        """
+        Saves file according to format.
+        :param name:
+        :param data:
+        :return:
+        """
         with open(self.get_filename(name), "w") as f:
             json.dump(data, f)
 
-    def load_file(self, name):
+    def load_file(self, name: str):
+        """
+        Loads file from name.
+        :param name:
+        :return:
+        """
         if not os.path.isfile(self.get_filename(name)):
             self.save_file(name, self.default(name))
         with open(self.get_filename(name), "r") as f:
