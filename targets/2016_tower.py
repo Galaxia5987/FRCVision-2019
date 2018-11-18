ce5f6563b99a4f15d00fb43af64785e722f05127
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-11-18 23:35:17 +0200

    Add 2016 Tower target

--- targets/2016_tower.py
+++ targets/2016_tower.py
@@ -1,27 +1,14 @@
 import cv2
 import numpy as np
 import utils
+from targets.target_base import TargetBase
 
 
-class Target:
-    def __init__(self, name):
-        self.name = name
-        self.kernel = np.array([[0, 1, 0],
-                                [1, 1, 1],
-                                [0, 1, 0]], dtype=np.uint8)
+class Target(TargetBase):
+    """Class representing the Tower light reflectors target from FIRST Stronghold."""
 
     @classmethod
     def filter_contours(cls, contours):
-        # create a cross kernel
-        mask = utils.morphology(mask, self.kernel)
-        mask = cv2.threshold(mask, 127, 255, 0)[1]
-        return mask
-
-    @staticmethod
-    def find_contours(mask):
-        im2, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        return contours
-
         correct_contours = []
         for cnt in contours:
             solidity = utils.solidity(cnt)
