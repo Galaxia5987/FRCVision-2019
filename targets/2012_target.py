f75108694baabe785739b01929f69f52d13997cf
yuval senderovitz <yuvalsan4545@gmail.com>
2018-12-06 13:33:44 +0200

    Better perfomance

--- targets/2012_target.py
+++ targets/2012_target.py
@@ -17,14 +17,20 @@ class Target(TargetBase):
     def filter_contours(contours, hierarchy):
         correct_contours = []
         for cnt in contours:
-            if cv2.contourArea(cnt) > 1000:
+            if cv2.contourArea(cnt) > 250:
                 children = utils.get_children(cnt, contours, hierarchy)
-                for c in children:
-                    area = cv2.contourArea(cnt)
-                    children_area = cv2.contourArea(c)
-                    ratio = children_area/area
-                    if 0.6 < ratio < 0.7:
+                if len(children) >= 1:
+                    for c in children:
+                        area = cv2.contourArea(cnt)
+                        children_area = cv2.contourArea(c)
+                        ratio = children_area/area
+                        if 0.57 < ratio < 0.73:
+                            correct_contours.append(cnt)
+                else:
+                    solidity = utils.solidity(cnt)
+                    if 0.15 < solidity < 0.45:
                         correct_contours.append(cnt)
+                        print(utils.solidity(cnt))
 
         return correct_contours
 
@@ -36,4 +42,4 @@ class Target(TargetBase):
             rect = cv2.minAreaRect(cnt)
             box = cv2.boxPoints(rect)
             box = np.int0(box)
-            cv2.drawContours(original, [box], 0, (0, 0, 255), 2)
+            cv2.drawContours(original, [box], 0, (255, 0, 255), 7)
