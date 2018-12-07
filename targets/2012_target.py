adcc095290aa640d4db287a8d1e90ae3a67d5090
yuval senderovitz <yuvalsan4545@gmail.com>
2018-12-07 12:34:10 +0200

    Improve mask, fix distance

--- targets/2012_target.py
+++ targets/2012_target.py
@@ -17,6 +17,16 @@ class Target(TargetBase):
         self.exposure = -10
 
     @staticmethod
+    def create_mask(frame, hsv):
+        mask = utils.hsv_mask(frame, hsv)
+        # create a cross kernel
+        mask = utils.morphology(mask, np.array([[0, 1, 0],
+                                                [1, 1, 1],
+                                                [0, 1, 0]], dtype=np.uint8))
+        mask = cv2.threshold(mask, 250, 255, 0)[1]
+        return mask
+
+    @staticmethod
     def filter_contours(contours, hierarchy):
         correct_contours = []
         all_children = []
@@ -53,8 +63,10 @@ class Target(TargetBase):
             (a, b), radius = cv2.minEnclosingCircle(box)
             center = int(a), int(b)
             cv2.circle(original, center, int(radius), (0, 0, 255), 5)
+            rect = cv2.minAreaRect(cnt)
             distance = utils.distance(constants.FOCAL['lifecam'],
-                                      constants.TARGET_SIZE['2012']['closing_circle_radius'], radius) * 100
+                                      constants.TARGET_SIZE['2012']['width'], max(rect[1][0], rect[1][1])) * 100 * 1.05
             angle = utils.angle(constants.FOCAL['lifecam'], int(a), original)
             font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(original, str(int(angle)), (int(a), int(b + radius)), font, 2, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(original, str(int(distance)), (int(a), int(b + radius)), font, 2, (255, 255, 255), 2,
+                        cv2.LINE_AA)
