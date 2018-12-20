f55e98a33fa5c4fab2190e267a204df37befc564
Repulser <thetechproject2014@gmail.com>
2018-12-20 09:02:38 +0200

    Better exposure level

--- targets/2012_target.py
+++ targets/2012_target.py
@@ -11,7 +11,7 @@ class Target(TargetBase):
 
     def __init__(self):
         super().__init__()
-        self.exposure = -10
+        self.exposure = -20
 
     @staticmethod
     def create_mask(frame, hsv):
