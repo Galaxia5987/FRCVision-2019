5823abc46e0286b780bf385bedee6827e58e70d4
Repulser <thetechproject2014@gmail.com>
2018-12-21 11:51:21 +0200

    Improve docs a little

--- display.py
+++ display.py
@@ -9,23 +9,19 @@ class Display:
     def get_frame(self):
         """
         Return the most current frame from the camera provider.
-        :return:
+        :return: Latest frame
         """
         return self.camera_provider.frame
 
-    def change_exposure(self, new_exposure):
+    def change_exposure(self, new_exposure: int):
         """
         Change the exposure through the camera provider.
-        :param new_exposure:
-        :return:
+        :param new_exposure: New exposure to set
         """
         self.camera_provider.set_exposure(new_exposure)
 
     def release(self):
-        """
-        Release the camera and destroys windows.
-        :return:
-        """
+        """Release the camera and destroys windows."""
         self.camera_provider.release()
         cv2.destroyAllWindows()
 
@@ -33,8 +29,7 @@ class Display:
     def show_frame(frame, title='image'):
         """
         Show frame to screen.
-        :param frame:
-        :param title:
-        :return:
+        :param frame: OpenCV frame
+        :param title: Window title
         """
         cv2.imshow(title, frame)
