728a3d838b5ec1bebd1d4b5811bf1b64047434b0
yuval senderovitz <yuvalsan4545@gmail.com>
2019-01-11 14:47:09 +0200

    Fix performance

--- realsense.py
+++ realsense.py
@@ -5,27 +5,26 @@ import numpy
 import pyrealsense2 as rs
 
 
-class Realsense(Thread):
+class Realsense():
     def __init__(self):
         config = rs.config()
-        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+        config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
+        config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
         self.pipeline = rs.pipeline()
         self.pipeline.start(config)
         self.exit = False
-        self.frame = None
         self.depth_frame = None
-        super().__init__(daemon=True)  # Init thread
 
-    def run(self):
+    @property
+    def frame(self):
+        frames = self.pipeline.wait_for_frames()
+        self.depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+        return numpy.asanyarray(color_frame.get_data())
+
+    def start(self):
         """Implementation of Thread run method, stores frames in a class variable."""
-        while True:
-            if self.exit:
-                break
-            frames = self.pipeline.wait_for_frames()
-            self.depth_frame = frames.get_depth_frame()
-            color_frame = frames.get_color_frame()
-            self.frame = numpy.asanyarray(color_frame.get_data())
+        pass
 
     def release(self):
         """Release the camera and loop."""
