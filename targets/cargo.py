76e54e51e28ed450df1b908129098afe46db1dbf
Repulser <thetechproject2014@gmail.com>
2019-01-13 21:42:49 +0200

    Improve cargo detection

--- targets/cargo.py
+++ targets/cargo.py
@@ -1,6 +1,6 @@
 import cv2
-import utils
 
+import utils
 from targets.target_base import TargetBase
 
 
@@ -10,17 +10,25 @@ class Target(TargetBase):
     @staticmethod
     def filter_contours(contours, hierarchy):
         correct_contours = []
-
+        all_children = []
         if contours:
             for cnt in contours:
-                if utils.is_circle(cnt, 0.85):
+                if cv2.contourArea(cnt) < 200:
+                    continue
+                if utils.is_circle(cnt, 0.7):
+                    all_children.extend(utils.get_children(cnt, contours, hierarchy))
                     correct_contours.append(cnt)
-
+        for cnt in all_children:
+            try:
+                correct_contours.remove(cnt)
+            except ValueError:
+                continue
         return correct_contours
 
     @staticmethod
     def draw_contours(filtered_contours, original):
         if filtered_contours:
             for cnt in filtered_contours:
-                cv2.drawContours(original, cnt, -1, (255, 255, 0), -1)
-                cv2.circle(original, utils.center(cnt), int(utils.width(cnt)[0] / 2), (0, 255, 0), -1)
+                (a, b), radius = cv2.minEnclosingCircle(cnt)
+                center = int(a), int(b)
+                cv2.circle(original, center, int(radius), (0, 255, 0), 5)
